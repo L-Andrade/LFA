4a663afff3beabbc71dd02a0fdd4415b8d2eba8e
JVictorRS <jvictor.reiss@gmail.com>
2018-04-19 18:49:19 +0100

    created extract_specific_key() to wer_extractor

--- werExtractor/wer_extractor.py
+++ werExtractor/wer_extractor.py
@@ -23,3 +23,18 @@ def extract_default_keys(pathToFile):
     for key in CONST_DEFAULT:
         res[key] = myDict[key]
     return res
+
+def extract_specific_key(pathToFile,key):
+    try:
+        f = codecs.open(pathToFile, 'r', encoding='utf-16le')
+        lines = f.readlines()
+        f.close()
+    except IOError:
+        return [-1]
+    myDict = {}
+    for line in lines:
+        sLines = line.split("=")
+        if(sLines[0] == key):
+            return sLines[1]
+    return ""
+                    
